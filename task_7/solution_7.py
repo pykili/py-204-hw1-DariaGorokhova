edefa86c80d6662d310240da9162410cd1e1641a
DariaGorokhova <72070081+DariaGorokhova@users.noreply.github.com>
2020-10-29 17:49:26 +0300

    Update solution_7.py

--- task_7/solution_7.py
+++ task_7/solution_7.py
@@ -1,1 +1,9 @@
 # your code here
+string = input()
+is_palindrom = False
+palindrom = ''
+for i in string:
+  palindrom = i + palindrom
+if string == palindrom:
+  is_palindrom = True
+print(is_palindrom)
