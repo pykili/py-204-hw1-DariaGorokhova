260fe6d20f6253a967bfad8f84d341ca25c6e340
DariaGorokhova <72070081+DariaGorokhova@users.noreply.github.com>
2020-10-29 17:51:20 +0300

    Update solution_10.py

--- task_10/solution_10.py
+++ task_10/solution_10.py
@@ -1,1 +1,9 @@
 # your code here
+string = input()
+is_palindrom = 'Not a palindrome'
+palindrom = ''
+for i in string:
+  palindrom = i + palindrom
+if string == palindrom:
+  is_palindrom = 'Palindrome'
+print(is_palindrom)
