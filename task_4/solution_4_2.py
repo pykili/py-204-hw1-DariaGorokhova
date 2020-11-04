a91bf8372f23822b4404a072c8ce257a1034db1a
DariaGorokhova <72070081+DariaGorokhova@users.noreply.github.com>
2020-11-04 22:33:24 +0300

    Update solution_4_2.py

--- task_4/solution_4_2.py
+++ task_4/solution_4_2.py
@@ -1,6 +1,24 @@
 # your code here
 for smth in 'a'*10:
-    user_input = input()
-    # your code here
-    if my_cool_condition == True:
-        print(form, lemma)
+  user_input = input()
+  numoftab = 0
+  form = ''
+  lemma = ''
+  if user_input == '' or user_input[0] == '#':
+    break
+  else:
+    for i in user_input:
+      if i == '\t':
+       numoftab += 1
+     if numoftab == 1:
+       form = form + i
+     if numoftab == 2:
+       lemma = lemma + i
+    numofletter = 0
+    equalpart = ''
+    for letter in lemma:
+      numofletter += 1
+      if lemma[numofletter] == form[numofletter]:
+        equalpart = equalpart + letter
+    if form != lemma and lemma != equalpart:
+      print (form, lemma)
